046ec574bad98869e1cf80b75e3f7f9be8df8f88
Andrey Pudov <Andrey_Pudov@epam.com>
2021-02-09 23:34:07 +0300

    ~ adds Little Russia article

--- src/components/albums/YoshkarOla.tsx
+++ src/components/albums/YoshkarOla.tsx
@@ -4,6 +4,66 @@ import Layout from './../shared/Layout'
 function YoshkarOla() {
   return (
     <Layout>
+      <div className = 'container'>
+        <div className = 'page-header'>
+          <h1>Yoshkar-Ola. Tsar's town on Kokshaga River<small><em>Saturday, May 10, 2014</em></small></h1>
+        </div>
+
+        <img src = '../albums/2014-05-10/2014-05-10_001_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-05-10/2014-05-10_002_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_003_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_004_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_005_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-05-10/2014-05-10_006_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_007_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_008_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-05-10/2014-05-10_009_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-05-10/2014-05-10_010_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_011_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_012_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-05-10/2014-05-10_013_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_014_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_015_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_016_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-05-10/2014-05-10_017_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_018_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_019_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-05-10/2014-05-10_020_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_021_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_022_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_023_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_024_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_025_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-05-10/2014-05-10_026_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-05-10/2014-05-10_027_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_028_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_029_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_030_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_031_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_032_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-05-10/2014-05-10_033_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_034_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_035_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_036_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-05-10/2014-05-10_037_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-05-10/2014-05-10_038_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_039_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_040_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-05-10/2014-05-10_041_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_042_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_043_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-05-10/2014-05-10_044_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-05-10/2014-05-10_045_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-05-10/2014-05-10_046_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-05-10/2014-05-10_047_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-05-10/2014-05-10_048_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_049_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-05-10/2014-05-10_050_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-05-10/2014-05-10_051_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-05-10/2014-05-10_052_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-05-10/2014-05-10_053_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-05-10/2014-05-10_054_large.jpg' alt = 'Andrey Pudov, Yoshkar-Ola, Йошкар-Ола. Царёв град на Кокшаге' className = 'img-responsive gallery-image' />
+      </div>
     </Layout>
   );
 }
