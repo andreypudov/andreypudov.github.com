1d16596f2b60aeed62afe0904cacc11ff449034e
Andrey Pudov <Andrey_Pudov@epam.com>
2021-02-09 23:21:04 +0300

    ~ adds early winter article

--- src/components/albums/EarlyWinter.tsx
+++ src/components/albums/EarlyWinter.tsx
@@ -4,6 +4,24 @@ import Layout from './../shared/Layout'
 function EarlyWinter() {
   return (
     <Layout>
+      <div className = 'container'>
+        <div className = 'page-header'>
+          <h1>Early Winter<small><em>Sunday, October 19, 2014</em></small></h1>
+        </div>
+
+        <img src = '../albums/2014-10-19/2014-10-19_001_large.jpg' alt = 'Andrey Pudov, Nizgny Novgorod, Early Winter' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-10-19/2014-10-19_002_large.jpg' alt = 'Andrey Pudov, Nizgny Novgorod, Early Winter' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-10-19/2014-10-19_003_large.jpg' alt = 'Andrey Pudov, Nizgny Novgorod, Early Winter' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-10-19/2014-10-19_004_large.jpg' alt = 'Andrey Pudov, Nizgny Novgorod, Early Winter' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-10-19/2014-10-19_005_large.jpg' alt = 'Andrey Pudov, Nizgny Novgorod, Early Winter' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-10-19/2014-10-19_006_large.jpg' alt = 'Andrey Pudov, Nizgny Novgorod, Early Winter' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-10-19/2014-10-19_007_large.jpg' alt = 'Andrey Pudov, Nizgny Novgorod, Early Winter' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-10-19/2014-10-19_008_large.jpg' alt = 'Andrey Pudov, Nizgny Novgorod, Early Winter' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-10-19/2014-10-19_009_large.jpg' alt = 'Andrey Pudov, Nizgny Novgorod, Early Winter' className = 'img-responsive gallery-image vertical' />
+        <img src = '../albums/2014-10-19/2014-10-19_010_large.jpg' alt = 'Andrey Pudov, Nizgny Novgorod, Early Winter' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-10-19/2014-10-19_011_large.jpg' alt = 'Andrey Pudov, Nizgny Novgorod, Early Winter' className = 'img-responsive gallery-image' />
+        <img src = '../albums/2014-10-19/2014-10-19_012_large.jpg' alt = 'Andrey Pudov, Nizgny Novgorod, Early Winter' className = 'img-responsive gallery-image' />
+      </div>
     </Layout>
   );
 }
