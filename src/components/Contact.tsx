34f4cb1af4a849cf542ff66f0d4877cb3cdbff36
Andrey Pudov <Andrey_Pudov@epam.com>
2021-02-21 15:27:39 +0300

    ~ improves header styling

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -11,6 +11,14 @@ function Contact() {
           Thank you for visiting the site, should you have been unable to find the information that you required or wish to find out any further information, please feel free to contact me and I will get back to you as soon as possible.
         </p>
 
+        <div className = 'card bg-dark text-white gallery-image'>
+          <img src = '../albums/2020-07-19_08-04/2020-07-19_08-04_099.jpg' className = 'card-img' alt = '' />
+          <div className = 'card-img-overlay'>
+            <h5 className = 'card-title'>Altay. Conquering Siberia 2.0</h5>
+            <p className = 'card-text'>Sunday, July 19, 2020</p>
+          </div>
+        </div>
+
         <address>
           <strong>Andrey Pudov</strong><br />
           <a href='mailto:javascript(0);'>mail@andreypudov.com</a>
