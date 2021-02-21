243f4b05f9392b9cbd65cb8157748dbf44db6a3f
Andrey Pudov <Andrey_Pudov@epam.com>
2021-02-21 15:27:39 +0300

    ~ improves header styling

--- src/components/Albums.tsx
+++ src/components/Albums.tsx
@@ -7,8 +7,8 @@ function Albums() {
       <div className = 'container'>
         <h1>Albums</h1>
 
-        <div className = 'card bg-dark text-white'>
-          <img src = '../albums/2020-07-19_08-04/2020-07-19_08-04_002.jpg' className = 'card-img' alt = '' />
+        <div className = 'card bg-dark text-white gallery-image'>
+          <img src = '../albums/2020-07-19_08-04/2020-07-19_08-04_099.jpg' className = 'card-img' alt = '' />
           <div className = 'card-img-overlay'>
             <h5 className = 'card-title'>Altay. Conquering Siberia 2.0</h5>
             <p className = 'card-text'>Sunday, July 19, 2020</p>
