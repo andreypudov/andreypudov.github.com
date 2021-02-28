111f884cbe11fbb0197ef11a498746bab20e7797
Andrey Pudov <Andrey_Pudov@epam.com>
2021-02-28 19:01:50 +0300

    ~ adds embedded music sheets

---
+++ src/models/performances/cureForWellness.ts
@@ -0,0 +1,9 @@
+import Video from '../core/video';
+import VideoAlbum from './../core/videoAlbum';
+
+export default new VideoAlbum(
+  'A Cure for Wellness',
+  new Date('2018-03-17'),
+  [
+    new Video('https://player.vimeo.com/video/517761510'),
+  ]);
---
+++ src/models/performances/miaAndSebastian.ts
@@ -0,0 +1,9 @@
+import Video from '../core/video';
+import VideoAlbum from './../core/videoAlbum';
+
+export default new VideoAlbum(
+  'Mia & Sebastian',
+  new Date('2017-09-07'),
+  [
+    new Video('https://player.vimeo.com/video/517760954'),
+  ]);
