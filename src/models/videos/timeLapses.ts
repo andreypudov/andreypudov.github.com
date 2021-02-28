fdc7e841919281b4d0feb1ee87d217890b82c127
Andrey Pudov <Andrey_Pudov@epam.com>
2021-02-28 16:38:07 +0300

    ~ migrate time lapses to vimeo

--- src/models/videos/timeLapses.ts
+++ src/models/videos/timeLapses.ts
@@ -5,10 +5,10 @@ export default new VideoAlbum(
   'Time Lapses',
   new Date('2017-04-14'),
   [
-    new Video('https://www.youtube.com/embed/uJzZqGmiRMI', 'Парк Швейцария'),
-    new Video('https://www.youtube.com/embed/v-HWYfvLLUo', 'Канавинский мост'),
-    new Video('https://www.youtube.com/embed/7Ujcu3o7bkY', 'Банк России'),
-    new Video('https://www.youtube.com/embed/TcC3kf0Mlzk', 'Площадь Максима Горького'),
-    new Video('https://www.youtube.com/embed/tGkIeioSP2E', 'Зеленский Съезд'),
-    new Video('https://www.youtube.com/embed/0YqJLA41UCs', 'Большая Покровская'),
+    new Video('https://player.vimeo.com/video/517763009'),
+    new Video('https://player.vimeo.com/video/517762962'),
+    new Video('https://player.vimeo.com/video/517762910'),
+    new Video('https://player.vimeo.com/video/517762848'),
+    new Video('https://player.vimeo.com/video/517762791'),
+    new Video('https://player.vimeo.com/video/517762737'),
   ]);
