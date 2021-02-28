fa7ea5de718da557e357e3c77e93aa238f178106
Andrey Pudov <Andrey_Pudov@epam.com>
2021-02-28 23:51:05 +0300

    ~ adds snapshot tests

---
+++ src/tests/components/photographs/ConqueringRome.test.tsx
@@ -0,0 +1,14 @@
+import { MemoryRouter } from 'react-router-dom'
+import Component from './../../../components/photographs/ConqueringRome';
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(
+      <MemoryRouter>
+        <Component />
+      </MemoryRouter>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
