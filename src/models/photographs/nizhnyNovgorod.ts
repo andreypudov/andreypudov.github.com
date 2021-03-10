ccdb0072eadc95dd45becc3eecb163b27a140a53
Andrey Pudov <Andrey_Pudov@epam.com>
2021-03-10 11:43:25 +0300

    ~ add photo to nizhny album

--- src/models/photographs/nizhnyNovgorod.ts
+++ src/models/photographs/nizhnyNovgorod.ts
@@ -9,6 +9,7 @@ export default new Album(
     new Photograph('../albums/2019-07-16/2019-07-16_002.jpg', true),
     new Photograph('../albums/2018-04-19/2018-04-19_001.jpg'),
     new Photograph('../albums/2018-04-19/2018-04-19_002.jpg'),
+    new Photograph('../albums/2012-03-17/2012-03-17_024_large.jpg'),
     new Photograph('../albums/2014-10-19/2014-10-19_010_large.jpg'),
     new Photograph('../albums/2014-10-19/2014-10-19_011_large.jpg'),
     new Photograph('../albums/2014-10-19/2014-10-19_012_large.jpg'),
