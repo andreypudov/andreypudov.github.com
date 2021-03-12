ba5c3eea246c547aa43d7f3f663103e87ca88fd1
Andrey Pudov <Andrey_Pudov@epam.com>
2021-03-12 12:17:02 +0300

    ~ updates table of content

--- src/components/photographs/AlinaCherdakova.tsx
+++ src/components/photographs/AlinaCherdakova.tsx
@@ -2,7 +2,6 @@ import Album from './../../models/photographs/alinaCherdakova';
 import AlbumView from '../shared/Album';
 import Headline from './../shared/Headline';
 import Layout from './../shared/Layout';
-import React from 'react';
 
 function AlinaCherdakova() {
   return (
