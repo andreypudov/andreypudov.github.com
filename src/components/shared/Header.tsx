329c4872634aee66eb35e368b37e8f4dcdd7adcd
Andrey Pudov <mail@andreypudov.com>
2021-03-16 21:16:31 +0300

    ~ improves mobile representation

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -4,16 +4,14 @@ function Header() {
   return (
     <header>
       <div className = 'container'>
-        <div className = 'navbar navbar-expand-lg navbar-dark'>
+        <div className = 'navbar navbar-expand-md navbar-dark'>
           <div className = 'container-fluid'>
+            <Link to = '/' className = 'navbar-brand mx-auto'>Andrey Pudov</Link>
             <button className = 'navbar-toggler' type = 'button' data-bs-toggle = 'collapse' data-bs-target = '#navbarMain' aria-controls = 'navbarMain' aria-expanded = 'false' aria-label = 'Toggle navigation'>
               <span className = 'navbar-toggler-icon'></span>
             </button>
-            <div className = 'collapse navbar-collapse justify-content-md-center' id = 'navbarMain'>
+            <div className = 'collapse navbar-collapse justify-content-sm-center' id = 'navbarMain'>
               <ul className = 'navbar-nav'>
-                <li className = 'nav-item'>
-                  <Link to = '/' className = 'nav-link'>Andrey Pudov</Link>
-                </li>
                 <li className = 'nav-item'>
                   <Link to = '/albums' className = 'nav-link'>Albums</Link>
                 </li>
