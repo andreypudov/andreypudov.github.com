de066b84a7d30cf597fac6517b011fc7f364854d
Andrey Pudov <mail@andreypudov.com>
2021-04-14 22:02:37 +0300

    ~ removes photos of Kate

--- src/models/drawings/pencilSketches.ts
+++ src/models/drawings/pencilSketches.ts
@@ -4,11 +4,11 @@ import Photograph from '../core/photograph';
 export default new Album(
   'Pencil Sketches',
   new Date('2020-07-16'),
-  new Photograph('../drawings/PencilSketches/WLmbtK_QN8g.jpg', false, 'April 26, 2020'),
+  new Photograph('../drawings/PencilSketches/lNr54EOZNxE.jpg', false, 'April 26, 2020'),
   [
     new Photograph('../drawings/PencilSketches/vaJwDlqhCMQ.jpg', true, 'July 16, 2020'),
     new Photograph('../drawings/PencilSketches/W6u08dHAbLg.jpg', true, 'July 9, 2020'),
-    new Photograph('../drawings/PencilSketches/WLmbtK_QN8g.jpg', false, 'April 26, 2020'),
+    // new Photograph('../drawings/PencilSketches/WLmbtK_QN8g.jpg', false, 'April 26, 2020'),
     new Photograph('../drawings/PencilSketches/iioBNj1E2Ts.jpg', true, 'April 22, 2020'),
     new Photograph('../drawings/PencilSketches/FDX1JlL5YWM.jpg', false, 'April 7, 2020'),
     new Photograph('../drawings/PencilSketches/lNr54EOZNxE.jpg', false, 'April 3, 2020'),
