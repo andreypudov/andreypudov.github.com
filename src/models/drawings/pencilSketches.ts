435e5a59e95e5120856d9562da90e6de8d7e1ce9
Andrey Pudov <mail@andreypudov.com>
2021-05-16 12:50:51 +0300

    ~ updates drawings

--- src/models/drawings/pencilSketches.ts
+++ src/models/drawings/pencilSketches.ts
@@ -4,13 +4,13 @@ import Photograph from '../core/photograph';
 export default new Album(
   'Pencil Sketches',
   new Date('2020-07-16'),
-  new Photograph('../drawings/PencilSketches/lNr54EOZNxE.jpg', false, 'April 26, 2020'),
+  new Photograph('../drawings/PencilSketches/2020-04-26.jpg', false, 'April 26, 2020'),
   [
-    new Photograph('../drawings/PencilSketches/vaJwDlqhCMQ.jpg', true, 'July 16, 2020'),
-    new Photograph('../drawings/PencilSketches/W6u08dHAbLg.jpg', true, 'July 9, 2020'),
-    // new Photograph('../drawings/PencilSketches/WLmbtK_QN8g.jpg', false, 'April 26, 2020'),
-    new Photograph('../drawings/PencilSketches/iioBNj1E2Ts.jpg', true, 'April 22, 2020'),
-    new Photograph('../drawings/PencilSketches/FDX1JlL5YWM.jpg', false, 'April 7, 2020'),
-    new Photograph('../drawings/PencilSketches/lNr54EOZNxE.jpg', false, 'April 3, 2020'),
-    new Photograph('../drawings/PencilSketches/RLaAhKlMXSA.jpg', true, 'April 2, 2020'),
+    new Photograph('../drawings/PencilSketches/2020-07-16.jpg', true, 'July 16, 2020'),
+    new Photograph('../drawings/PencilSketches/2020-07-09.jpg', true, 'July 9, 2020'),
+    new Photograph('../drawings/PencilSketches/2020-04-26.jpg', false, 'April 26, 2020'),
+    new Photograph('../drawings/PencilSketches/2020-04-22.jpg', true, 'April 22, 2020'),
+    new Photograph('../drawings/PencilSketches/2020-04-07.jpg', false, 'April 7, 2020'),
+    new Photograph('../drawings/PencilSketches/2020-04-03.jpg', false, 'April 3, 2020'),
+    new Photograph('../drawings/PencilSketches/2020-04-02.jpg', true, 'April 2, 2020'),
   ]);
