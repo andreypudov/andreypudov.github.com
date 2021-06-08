ca90954c2ed7aa47fd4e9f97e763704711f9d1f6
Andrey Pudov <mail@andreypudov.com>
2021-06-08 13:30:56 +0300

    ~ converts photos to webp format

--- src/models/photographs/balloonFestival.ts
+++ src/models/photographs/balloonFestival.ts
@@ -4,10 +4,10 @@ import Photograph from '../core/photograph';
 export default new Album(
   'Balloon Festival',
   new Date('2018-02-24'),
-  new Photograph('../albums/2018-02-24/2018-02-24_002.jpg'),
+  new Photograph('../albums/2018-02-24/2018-02-24_002.webp'),
   [
-    new Photograph('../albums/2018-02-24/2018-02-24_001.jpg'),
-    new Photograph('../albums/2018-02-24/2018-02-24_002.jpg'),
-    new Photograph('../albums/2018-02-24/2018-02-24_003.jpg'),
-    new Photograph('../albums/2018-02-24/2018-02-24_004.jpg'),
+    new Photograph('../albums/2018-02-24/2018-02-24_001.webp'),
+    new Photograph('../albums/2018-02-24/2018-02-24_002.webp'),
+    new Photograph('../albums/2018-02-24/2018-02-24_003.webp'),
+    new Photograph('../albums/2018-02-24/2018-02-24_004.webp'),
   ]);
--- src/models/photographs/nizhnyNovgorod.ts
+++ src/models/photographs/nizhnyNovgorod.ts
@@ -4,14 +4,14 @@ import Photograph from '../core/photograph';
 export default new Album(
   'Nizhny Novgorod',
   new Date('2019-07-16'),
-  new Photograph('../albums/2014-10-19/2014-10-19_012_large.jpg'),
+  new Photograph('../albums/2014-10-19/2014-10-19_012_large.webp'),
   [
-    new Photograph('../albums/2019-07-16/2019-07-16_001.jpg', true),
-    new Photograph('../albums/2019-07-16/2019-07-16_002.jpg', true),
-    new Photograph('../albums/2018-04-19/2018-04-19_001.jpg'),
-    new Photograph('../albums/2018-04-19/2018-04-19_002.jpg'),
-    new Photograph('../albums/2012-03-17/2012-03-17_024_large.jpg'),
-    new Photograph('../albums/2014-10-19/2014-10-19_010_large.jpg'),
-    new Photograph('../albums/2014-10-19/2014-10-19_011_large.jpg'),
-    new Photograph('../albums/2014-10-19/2014-10-19_012_large.jpg'),
+    new Photograph('../albums/2019-07-16/2019-07-16_001.webp', true),
+    new Photograph('../albums/2019-07-16/2019-07-16_002.webp', true),
+    new Photograph('../albums/2018-04-19/2018-04-19_001.webp'),
+    new Photograph('../albums/2018-04-19/2018-04-19_002.webp'),
+    new Photograph('../albums/2012-03-17/2012-03-17_024_large.webp'),
+    new Photograph('../albums/2014-10-19/2014-10-19_010_large.webp'),
+    new Photograph('../albums/2014-10-19/2014-10-19_011_large.webp'),
+    new Photograph('../albums/2014-10-19/2014-10-19_012_large.webp'),
   ]);
