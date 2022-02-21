864b2f4af973dc0ee455d8572c2843e914a0a12e
Andrey Pudov <mail@andreypudov.com>
2022-02-21 16:58:19 +0100

    add description to Marina Alekseeva album

--- src/components/photographs/MarinaAlekseeva.tsx
+++ src/components/photographs/MarinaAlekseeva.tsx
@@ -8,6 +8,9 @@ function MarinaAlekseeva() {
     <Layout title = { Album.getTitle() }>
       <div className = 'container'>
         <Headline album = { Album } />
+
+        <p>Marina is a former vice president in the Software and Services Group and serves as general manager for the group. Based in Nizhny Novgorod, she managed Intel research and development operations in Russia and was responsible for aligning them with Intel's business strategy. Today she is co-lead of the Women in Big Data program in EMEA.</p>
+
         <AlbumView album = { Album } />
       </div>
     </Layout>
