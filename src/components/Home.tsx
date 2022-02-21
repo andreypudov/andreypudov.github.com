98b6c140ec8949604e4b5073810b8ad0ca6cdaf8
Andrey Pudov <mail@andreypudov.com>
2022-02-21 18:48:02 +0100

    ~ update home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -24,16 +24,16 @@ function Home() {
               <h5>Altay. Conquering Siberia 2.0</h5>
             </div>
           </Link>
+          <Link to = '/photographs/nizhny-novgorod' className = 'carousel-item'>
+            <img src = 'images/carousel/nizhny-novgorod.webp' alt = 'Nizhny Novgorod' className = 'd-block w-100' />
+            <div className = 'carousel-caption d-none d-md-block text-dark'>
+              <h5>Nizhny Novgorod</h5>
+            </div>
+          </Link>
           <Link to = '/photographs/rekshino' className = 'carousel-item'>
             <img src = 'images/carousel/rekshino.webp' alt = 'Rekshino' className = 'd-block w-100' />
             <div className = 'carousel-caption d-none d-md-block'>
               <h5>Rekshino</h5>
-            </div>
-          </Link>
-          <Link to = '/photographs/alina-cherdakova' className = 'carousel-item'>
-            <img src = 'images/carousel/alina-cherdakova.webp' alt = 'Alina Cherdakova' className = 'd-block w-100' />
-            <div className = 'carousel-caption d-none d-md-block text-dark'>
-              <h5>Alina Cherdakova</h5>
             </div>
           </Link>
         </div>
@@ -55,18 +55,18 @@ function Home() {
             </Link>
           </div>
           <div className = 'col-sm-3'>
+            <Link to = '/drawings/pencil-sketches'>
+              <img src = 'images/marketing/pencil-sketches.webp' className = 'img-fluid' alt = 'Pencil Sketches' />
+            </Link>
+          </div>
+          <div className = 'col-sm-3'>
+            <Link to = '/drawings/soft-pastel'>
+              <img src = 'images/marketing/soft-pastel.webp' className = 'img-fluid' alt = 'Soft Pastel' />
+            </Link>
+          </div>
+          <div className = 'col-sm-3'>
             <Link to = '/photographs/rome-malta'>
               <img src = 'images/marketing/rome-malta.webp' className = 'img-fluid' alt = 'Conquering Rome and Order of Malta' />
-            </Link>
-          </div>
-          <div className = 'col-sm-3'>
-            <Link to = '/photographs/kira-orlova'>
-              <img src = 'images/marketing/kira-orlova.webp' className = 'img-fluid' alt = 'Kira Orlova' />
-            </Link>
-          </div>
-          <div className = 'col-sm-3'>
-            <Link to = '/photographs/united-states-2.0'>
-              <img src = 'images/marketing/united-states-2.0.webp' className = 'img-fluid' alt = 'Conquering America 2.0' />
             </Link>
           </div>
         </div>
