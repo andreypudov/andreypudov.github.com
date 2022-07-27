061790e696c964a87a0c97188e83b08bc94cd0cf
Andrey Pudov <mail@andreypudov.com>
2022-07-27 10:43:10 +0200

    ~ updated paster and watercolor albums

--- src/models/drawings/softPastel.ts
+++ src/models/drawings/softPastel.ts
@@ -7,6 +7,9 @@ export default new Album(
   new Photograph('/drawings/soft-pastel/2021-01-30.webp', false, 'January 30, 2021'),
   [
     new Photograph('/drawings/soft-pastel/2021-11-01.webp', false, 'November 01, 2021'),
+    new Photograph('/drawings/soft-pastel/2021-05-17.webp', true, 'May 17, 2021'),
+    new Photograph('/drawings/soft-pastel/2021-03-22.webp', true, 'March 22, 2021'),
+    new Photograph('/drawings/soft-pastel/2021-03-21.webp', true, 'March 21, 2021'),
     new Photograph('/drawings/soft-pastel/2021-01-30.webp', false, 'January 30, 2021'),
     new Photograph('/drawings/soft-pastel/2021-01-29.webp', true, 'January 29, 2021'),
     new Photograph('/drawings/soft-pastel/2021-01-07.webp', true, 'January 07, 2021'),
--- src/models/drawings/watercolor.ts
+++ src/models/drawings/watercolor.ts
@@ -6,6 +6,6 @@ export default new Album(
   new Date('2021-05-24'),
   new Photograph('/drawings/watercolor/2021-05-24.webp', false, 'May 05, 2021'),
   [
-    new Photograph('/drawings/watercolor/2021-03-30.webp', true, 'March 03, 2021'),
     new Photograph('/drawings/watercolor/2021-05-24.webp', false, 'May 05, 2021'),
+    new Photograph('/drawings/watercolor/2021-03-30.webp', true, 'March 03, 2021'),
   ]);
