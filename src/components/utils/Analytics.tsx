38c072b166edb4242a45fa259fa91ca44f00ab3a
Andrey Pudov <mail@andreypudov.com>
2022-11-13 00:11:44 +0100

    ~ upgraded to ga4

--- src/components/utils/Analytics.tsx
+++ src/components/utils/Analytics.tsx
@@ -1,13 +1,13 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
 export default function Analytics() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    ReactGA.pageview(pathname);
+    ReactGA.send({ hitType: 'pageview', page: pathname });
   }, [pathname]);
 
   return null;
