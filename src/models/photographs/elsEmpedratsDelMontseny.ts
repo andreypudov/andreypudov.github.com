05432aeaf6ca9bdb4bbc05f404724fb1c7e82a00
Andrey Pudov <mail@andreypudov.com>
2022-11-13 19:30:18 +0100

    ~ added els-empedrats-del-montseny album

---
+++ src/models/photographs/elsEmpedratsDelMontseny.ts
@@ -0,0 +1,21 @@
+import Album from '../core/photographyAlbum';
+import Photograph from '../core/photograph';
+
+export default new Album(
+  'Els Empedrats del Montseny',
+  new Date('2022-11-13'),
+  new Photograph('/photographs/2022-11-13/2022-11-13_003.webp'),
+  [
+    new Photograph('/photographs/2022-11-13/2022-11-13_001.webp', true),
+    new Photograph('/photographs/2022-11-13/2022-11-13_002.webp'),
+    new Photograph('/photographs/2022-11-13/2022-11-13_003.webp'),
+    new Photograph('/photographs/2022-11-13/2022-11-13_004.webp', true),
+    new Photograph('/photographs/2022-11-13/2022-11-13_005.webp', true),
+    new Photograph('/photographs/2022-11-13/2022-11-13_006.webp'),
+    new Photograph('/photographs/2022-11-13/2022-11-13_007.webp', true),
+    new Photograph('/photographs/2022-11-13/2022-11-13_008.webp', true),
+    new Photograph('/photographs/2022-11-13/2022-11-13_009.webp', true),
+    new Photograph('/photographs/2022-11-13/2022-11-13_010.webp', true),
+    new Photograph('/photographs/2022-11-13/2022-11-13_011.webp'),
+    new Photograph('/photographs/2022-11-13/2022-11-13_012.webp'),
+  ]);
