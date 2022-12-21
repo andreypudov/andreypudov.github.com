58785e578dfb3079c503501086aa2ded10e2a45a
Andrey Pudov <mail@andreypudov.com>
2022-12-21 19:09:40 +0100

    ~ added image specification scheme to sitemap

--- src/components/Sitemap.tsx
+++ src/components/Sitemap.tsx
@@ -21,7 +21,7 @@ const imageEntry = (pageTitle: string, media: Media) => `<image:image>
 const generateSitemap = (timestamp: string) => {
   return `
 <?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
   <url>
     <loc>https://andreypudov.com/</loc>
     <lastmod>${ timestamp }</lastmod>
