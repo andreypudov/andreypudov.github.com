391ecf200c437d20c74e37c4e1064a7b8e7ce17c
Andrey Pudov <mail@andreypudov.com>
2023-01-01 14:43:40 +0100

    ~ set page description in relation to the page

--- src/components/shared/Title.tsx
+++ src/components/shared/Title.tsx
@@ -6,7 +6,7 @@ type TitleProps = {
 
 function Title(props: TitleProps) {
   const title = props.title ? `${props.title} - Andrey Pudov` : 'Andrey Pudov';
-  const description = !props.title ? title : 'Official Andrey Pudov website featuring Andrey Pudov news, photo albums and more.';
+  const description = props.title ? props.title : 'Official Andrey Pudov website featuring Andrey Pudov news, photo albums and more.';
 
   return (
     <>
