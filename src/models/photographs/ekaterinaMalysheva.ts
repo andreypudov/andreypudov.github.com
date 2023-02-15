6960d9d386a17a0c43a99cef0c693a0dc0dd37fe
Andrey Pudov <mail@andreypudov.com>
2023-02-15 10:19:32 +0100

    ~ updated portrait albums

--- src/models/photographs/ekaterinaMalysheva.ts
+++ src/models/photographs/ekaterinaMalysheva.ts
@@ -6,10 +6,11 @@ export default new Album(
   new Date('2017-10-07'),
   new Photograph('/photographs/2016-04-16/2016-04-16_009.webp'),
   [
-    new Photograph('/photographs/2017-10-07/2017-10-07_001.webp'),
+    new Photograph('/photographs/2017-10-28_2/2017-10-28_001.webp', true),
+    // new Photograph('/photographs/2017-10-07/2017-10-07_001.webp'),
     new Photograph('/photographs/2017-10-07/2017-10-07_002.webp', true),
     new Photograph('/photographs/2017-10-07/2017-10-07_003.webp', true),
-    new Photograph('/photographs/2017-10-07/2017-10-07_004.webp', true),
-    new Photograph('/photographs/2017-10-07/2017-10-07_005.webp'),
+    // new Photograph('/photographs/2017-10-07/2017-10-07_004.webp', true),
+    // new Photograph('/photographs/2017-10-07/2017-10-07_005.webp'),
     new Photograph('/photographs/2016-04-16/2016-04-16_009.webp'),
   ]);
