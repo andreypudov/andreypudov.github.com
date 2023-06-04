408019a73f4369675892a2be31df8da6b1349912
Andrey Pudov <mail@andreypudov.com>
2023-06-04 16:52:10 +0200

    ~ updated sitemap

--- src/components/Sitemap.tsx
+++ src/components/Sitemap.tsx
@@ -4,8 +4,10 @@ import Page from './../models/core/page';
 import { Articles, Photographs, Drawings, TimeLapses, Performances } from './../models/pages';
 
 const domain = 'https://andreypudov.com';
-const urlEntry = (page: Page, timestamp: string) => `<url>
-    <loc>${domain}${ page.getRoute() }</loc>
+const supportedLanguages = ['en', 'ru'];
+
+const urlEntry = (page: Page, timestamp: string, locale: string) => `<url>
+    <loc>${domain}/${locale}${ page.getRoute() }</loc>
     ${ page.getAlbum().getMedia().map((media: Media) =>  imageEntry(page.getAlbum().getTitle(), media)).join("").trim() }
     <lastmod>${ timestamp }</lastmod>
   </url>
@@ -26,35 +28,34 @@ const encodeXml = (xml: string) => xml
   .replace(/"/g, '&quot;')
   .replace(/'/g, '&apos;');
 
-const generateSitemap = (timestamp: string) => {
-  return `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+const generateSitemap = (timestamp: string, locale: string) => {
+  return `
   <url>
-    <loc>https://andreypudov.com/</loc>
+    <loc>https://andreypudov.com/${locale}/</loc>
     <lastmod>${ timestamp }</lastmod>
   </url>
   <url>
-    <loc>https://andreypudov.com/albums/</loc>
+    <loc>https://andreypudov.com/${locale}/albums/</loc>
     <lastmod>${ timestamp }</lastmod>
   </url>
   <url>
-    <loc>https://andreypudov.com/contents/</loc>
+    <loc>https://andreypudov.com/${locale}/contents/</loc>
     <lastmod>${ timestamp }</lastmod>
   </url>
   <url>
-    <loc>https://andreypudov.com/resume/</loc>
+    <loc>https://andreypudov.com/${locale}/resume/</loc>
     <lastmod>${ timestamp }</lastmod>
   </url>
   <url>
-    <loc>https://andreypudov.com/contact/</loc>
+    <loc>https://andreypudov.com/${locale}/contact/</loc>
     <lastmod>${ timestamp }</lastmod>
   </url>
-  ${ Articles.map((page: Page) => urlEntry(page, timestamp)).join("").trim() }
-  ${ Photographs.map((page: Page) => urlEntry(page, timestamp)).join('').trim() }
-  ${ Drawings.map((page: Page) => urlEntry(page, timestamp)).join('').trim() }
-  ${ TimeLapses.map((page: Page) => urlEntry(page, timestamp)).join('').trim() }
-  ${ Performances.map((page: Page) => urlEntry(page, timestamp)).join('').trim() }
-</urlset>`;
+  ${ Articles.map((page: Page) => urlEntry(page, timestamp, locale)).join("").trim() }
+  ${ Photographs.map((page: Page) => urlEntry(page, timestamp, locale)).join('').trim() }
+  ${ Drawings.map((page: Page) => urlEntry(page, timestamp, locale)).join('').trim() }
+  ${ TimeLapses.map((page: Page) => urlEntry(page, timestamp, locale)).join('').trim() }
+  ${ Performances.map((page: Page) => urlEntry(page, timestamp, locale)).join('').trim() }
+  `;
 }
 
 const Sitemap = () => {
@@ -65,7 +66,12 @@ const Sitemap = () => {
         <h1>Sitemap</h1>
 
         <textarea style={ { width: '100%', height: '640px', border: '1px solid grey' } }>
-          { generateSitemap(timestamp) }
+          {
+            '<?xml version="1.0" encoding="UTF-8"?>'
+            + '\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">'
+            + supportedLanguages.map(locale => generateSitemap(timestamp, locale)).join('')
+            + '</urlset>'
+          }
         </textarea>
       </div>
     </Layout>
