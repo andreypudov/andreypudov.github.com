ad052cbf5855b09c63afa16779466833df925376
Andrey Pudov <mail@andreypudov.com>
2023-06-05 15:20:09 +0200

    ~ updated tests

--- src/i18nTests.ts
+++ src/i18nTests.ts
@@ -1,23 +1,16 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import Backend from "i18next-http-backend";
-
-const backend = new Backend({
-   loadPath: '/locales/{{lng}}/translation.json'
- });
 
 i18n
-  .use(Backend)
   .use(initReactI18next)
   .init({
     lng: 'en',
     fallbackLng: 'en',
     debug: true,
-    backend: backend,
     interpolation: {
       escapeValue: false
     },
-    resources: { en: { translationsNS: {} } },
+    resources: { en: { }, ru: { } },
   });
 
 export default i18n;
