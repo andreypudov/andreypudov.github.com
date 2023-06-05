0981f28bc4e5ded39bf472f2ab6c4da314656db1
Andrey Pudov <mail@andreypudov.com>
2023-06-05 13:11:19 +0200

    ~ added locales for footer

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next';
 import LocalizedLink from './LocalizedLink';
 
 type FooterProps = {
@@ -5,20 +6,22 @@ type FooterProps = {
 };
 
 const Footer = ({ landing }: FooterProps) => {
+  const { t } = useTranslation();
+
   return (
     <footer>
       <div className = 'container'>
         { !landing && <hr className = 'footer-divider' /> }
 
         <ul className = 'piped-links float-end'>
-          <li className = 'first piped-links-short'><LocalizedLink to = '/resume/'>About</LocalizedLink></li>
-          <li className = 'first piped-links-long'><LocalizedLink to = '/resume/'>About Andrey Pudov</LocalizedLink></li>
-          <li><LocalizedLink to = '/contact/'>Contact</LocalizedLink></li>
+          <li className = 'first piped-links-short'><LocalizedLink to = '/resume/'>{t('Footer.AboutShort')}</LocalizedLink></li>
+          <li className = 'first piped-links-long'><LocalizedLink to = '/resume/'>{t('Footer.AboutLong')}</LocalizedLink></li>
+          <li><LocalizedLink to = '/contact/'>{t('Footer.Contact')}</LocalizedLink></li>
         </ul>
 
         <ul className = 'piped-links'>
-          <p className = 'piped-links-long'>Copyright &#169; 2023 Andrey Pudov. All rights reserved.</p>
-          <p className = 'piped-links-short'>&#169; 2023 Andrey Pudov. All rights reserved.</p>
+          <p className = 'piped-links-long'>{t('Footer.CopyrightLong')}</p>
+          <p className = 'piped-links-short'>{t('Footer.CopyrightShort')}</p>
         </ul>
       </div>
     </footer>
