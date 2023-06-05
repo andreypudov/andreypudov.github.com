d8068a7de74f8ccb9d48c2fb5e34ef5d95fdbdae
Andrey Pudov <mail@andreypudov.com>
2023-06-05 13:36:28 +0200

    ~ added locales for contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,14 +7,14 @@ const Contact = () => {
   return (
     <Layout title = { 'Contact' }>
       <div className = 'container'>
-        <h1>Contact</h1>
+        <h1>{t('Contact.Contact')}</h1>
 
         <p>
-          Thank you for visiting the site, should you have been unable to find the information that you required or wish to find out any further information, please feel free to contact me and I will get back to you as soon as possible.
+          {t('Contact.ThankYou')}
         </p>
 
         <address>
-          <strong>Andrey Pudov</strong><br />
+          <strong>{t('Contact.AndreyPudov')}</strong><br />
           <a href='mailto:javascript(0);'>mail@andreypudov.com</a>
         </address>
       </div>
