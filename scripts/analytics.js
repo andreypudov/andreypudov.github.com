17fe7c456b82b454d4162fe4c7dc60fdac3241bd
Andrey Pudov <mail@andreypudov.com>
2025-12-09 12:08:28 +0100

    ~ improved google analytics

--- scripts/analytics.js
+++ scripts/analytics.js
@@ -1,7 +1,27 @@
+const GA_ID = 'G-E0Y9W77G5B';
+
 window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
+function gtag() { window.dataLayer.push(arguments); }
+
+function loadAnalytics() {
+  if (loadAnalytics.loaded) return;
+  loadAnalytics.loaded = true;
+
+  const script = document.createElement('script');
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
+  script.async = true;
+  script.onload = () => {
+    gtag('js', new Date());
+    gtag('config', GA_ID);
+  };
+  document.head.appendChild(script);
 }
 
-gtag('js', new Date());
-gtag('config', 'G-E0Y9W77G5B');
+const events = ['mousemove', 'scroll', 'touchstart', 'keydown', 'click'];
+events.forEach(e =>
+  window.addEventListener(e, loadAnalytics, { passive: true, once: true })
+);
+
+window.addEventListener('load', () =>
+  setTimeout(loadAnalytics, 1000)
+);
